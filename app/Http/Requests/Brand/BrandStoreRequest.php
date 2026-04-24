543693e84f3e5634ea391673a4b48d7f7755765a
1508opossum1987 <1508opossum1987@gmail.com>
2026-04-24 20:48:46 +0700

    edit blades and crud for products

---
+++ app/Http/Requests/Brand/BrandStoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Brand;
+
+use App\Rules\RussianCharsRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BrandStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:255|unique:brands,name', new RussianCharsRule(70, "Название бренда"),
+            'active' => 'sometimes|boolean',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Поле Name обязательно к заполнению!',
+            'name.min' => 'Минимальное количество символов 3!'
+        ];
+    }
+}
---
+++ app/Http/Requests/Country/CountryStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Country;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CountryStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:255|unique:countries,name', new CountryStoreRequest(70, request: "Название страны"),
+            'active' => 'sometimes|boolean',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Поле Name обязательно к заполнению!',
+            'name.min' => 'Минимальное количество символов 3!'
+        ];
+    }
+}
