909067a273c7e4fcf6739f90f22ca3155a0fee07
1508opossum1987 <1508opossum1987@gmail.com>
2026-05-06 00:00:07 +0700

    Filtration on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,29 @@ class HomeController extends Controller
             ->orWhere('description', 'like', '%' . $request->get('search') . '%');
         }
 
+        if ($request->has('sort'))
+        {
+            switch ($request->get('sort')){
+                case 'price_asc':
+                    $query->orderBy('price');
+                    break;
+                case 'price_desc':
+                    $query->orderByDesc('price');
+                    break;
+                case 'name_asc':
+                    $query->orderBy('name');
+                    break;
+                case 'name_desc':
+                    $query->orderByDesc('name');
+                    break;
+                case 'newest':
+                    $query->orderByDesc('created_at');
+                    break;
+                case 'oldest':
+                    $query->orderBy('created_at');
+                    break;
+            }
+        }
         return view('home', ['products' => $query
             ->with(['category', 'brand', 'country'])
             ->paginate(self::ITEMS_PER_PAGE)
